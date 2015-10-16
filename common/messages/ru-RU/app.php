ef5c3cde552e57cc58a1e9dc2855d42579f18007
Per1phery <21Denis91@mail.ru>
2015-10-16 10:43:57 -0400

    messages (i18n) updated

---
+++ common/messages/ru-RU/app.php
@@ -0,0 +1,9 @@
+<?php
+return [
+    'Install' => 'Установить',
+    'Remove' => 'Удалить',
+    'Module has been saved' => 'Модуль сохранен',
+    'Module has not been saved' => 'Модуль не был сохранен!',
+    'Module has been removed' => 'Модуль был удален!',
+    'Module has not been removed' => 'Модуль не был удален',
+];
